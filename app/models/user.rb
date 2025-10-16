a5492b58f732b595b5c55a8e993b695f8a7493a8
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-16 15:24:39 +0900

    deviseに関するビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_kana_name, presence: true
+  validates :first_kana_name, presence: true
+  validates :birthday, presence: true
+
 end
