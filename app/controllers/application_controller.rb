dd46833e957e7e03f041b4adcfc5b4603d92c8fd
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-16 22:40:41 +0900

    テストコード作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
 
   private
 
@@ -12,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :last_name, :first_name, :last_kana_name, :first_kana_name, :birthday ])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_kana_name, :first_kana_name, :birthday])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々ー]+\z/
+  VALID_KANA_REGEX = /\A[ァ-ヴー]+\z/
+
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_kana_name, presence: true
-  validates :first_kana_name, presence: true
   validates :birthday, presence: true
 
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字を両方含む必要があります' }
+
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+
+  validates :last_kana_name, presence: true, format: { with: VALID_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+  validates :first_kana_name, presence: true, format: { with: VALID_KANA_REGEX, message: 'は全角カタカナで入力してください' }
 end
