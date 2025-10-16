b04198b8e97543ca533fcc38bcb0ced3f0f1ff33
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-16 13:08:07 +0900

    usersテーブルの作成（修正）

--- db/migrate/20251016034829_devise_create_users.rb
+++ db/migrate/20251016034829_devise_create_users.rb
@@ -4,14 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, unique: true, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname            null: false
-      t.string :last_name           null: false
-      t.string :first_name          null: false
-      t.string :last_kana_name      null: false
-      t.string :first_kana_name     null: false
-      t.date   :birthday            null: false
+      t.string :nickname,           null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_kana_name,     null: false
+      t.string :first_kana_name,    null: false
+      t.date   :birthday,           null: false
+      
 
       ## Recoverable
       t.string   :reset_password_token
