ce1bb1f1181e46fe5b32223e8ce3e5274a5cbe60
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-21 12:55:55 +0900

    商品一覧表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [ :new, :create ]
+  resources :items, only: [ :new, :create, :index ]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,4 +1,4 @@
 FactoryBot.define do
-  factory :prefecture do
+  factory :order do
   end
 end
