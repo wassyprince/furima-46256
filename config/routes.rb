12da465f507eb0e0e54711aa3132ff5d0abcf9e7
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-22 11:27:33 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [ :new, :create, :index ]
+  resources :items, only: [ :new, :create, :index, :show, :edit ]
+  
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
