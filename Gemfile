c951595a4d94cea6ef832d5b6b0e6380fc92b5c6
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-22 19:20:42 +0900

    商品情報編集機能

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ end
 gem 'devise'
 
 gem 'active_hash'
+
+gem 'pry-rails'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :set_item, only: [ :show, :edit, :update]
+  before_action :redirect_if_not_owner, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -21,10 +23,30 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
+  end
+  
+  def edit
+  
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_if_not_owner
+    redirect_to root_path unless current_user&.id == @item.user_id
+  end
 
   def item_params
     params.require(:item).permit(
