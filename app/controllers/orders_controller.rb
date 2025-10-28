64ad13bc3858e4d0438fba39cf3d1d39bb2c4289
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-28 14:12:37 +0900

    商品購入機能（修正2回目）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,6 @@ class OrdersController < ApplicationController
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
