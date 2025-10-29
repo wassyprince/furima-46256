9c4d4f3c217eb81c2cb7eeaad65cc9d2a6cc533a
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-10-29 15:32:09 +0900

    終了報告（修正1回目）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,7 @@ class OrdersController < ApplicationController
       @order_address.save
       return redirect_to root_path
     else
-      gon.public_key = ENV["PAYJP_SECRET_KEY"]
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render 'index', status: :unprocessable_entity
     end
   end
